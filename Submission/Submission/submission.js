b90a6ff3b06eb898fe99f483dafd3835dd4fe9c8
Maria Stephanie <mariastephanie2002@gmail.com>
2024-09-22 15:13:09 +0700

    Add files via upload

---
+++ Submission/Submission/submission.js
@@ -0,0 +1,9 @@
+document.getElementById("tombol1").onclick = function(){
+    alert("Tentang Saya")
+}
+document.getElementById("tombol2").onclick = function(){
+    alert("Pengalaman Saya")
+}
+document.getElementById("tombol3").onclick = function(){
+    alert("Pendidikan Saya")
+}
